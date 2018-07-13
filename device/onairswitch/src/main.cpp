5d76bf39b62da242ad75acfe57a27e9d93be6fdc
Sterling Hanenkamp <sterling@hanenkamp.com>
2018-07-13 00:57:05 -0500

    Adding Simulator support to the onairswitch

--- device/onairswitch/src/main.cpp
+++ device/onairswitch/src/main.cpp
@@ -1,22 +1,31 @@
-#include <WiFiUdp.h>
+#ifdef ARDUINO
 #include <Wire.h>
 
 #include <LED.h>
 #include <MC_Logger.h>
 #include <MC_Network.h>
+#else//ARDUINO
+#include <SimWire.h>
+
+#include <SimLED.h>
+#include <SimLogger.h>
+#include <Network.h>
+#endif//ARDUINO
 
 #include "on-air-switch.h"
 #include "secrets.h"
 
 #define PIN_CONNECT 2
 
-unsigned int local_udp_port = 10101;
-char incoming[500];
-bool flipflop_connected = false;
-
+#ifdef ARDUINO
 mysook::MC_Logger<Print> logger(&micros, Serial);
 mysook::MC_Network network(logger);
 mysook::LED status_light(PIN_CONNECT);
+#else//ARDUIONO
+mysook::SimLogger logger;
+mysook::Network network;
+mysook::SimLED status_light;
+#endif//ARDUINO
 
 OnAirSwitch on_air_switch(logger, network, status_light);
 
